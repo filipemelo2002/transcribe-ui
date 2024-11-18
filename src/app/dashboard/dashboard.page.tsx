e672f90c69b5584831ecd530b3c6ca46f4cbe83b
Filipe Melo <filipe.silva@encora.com>
2024-11-18 16:36:59 -0300

    feat: change audio to video & display captions

--- src/app/dashboard/dashboard.page.tsx
+++ src/app/dashboard/dashboard.page.tsx
@@ -9,34 +9,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { TranscribeAIService } from "@/services/transcribe-ai.service";
+import { SubtitleService } from "@/services/subtitle.service";
+import { Segment, TranscribeAIService } from "@/services/transcribe-ai.service";
 import { AudioLines, Loader, Trash } from "lucide-react";
 import { ChangeEvent, useRef, useState } from "react";
 
+const subtitleService = new SubtitleService()
+
 export const Dashboard = () => {
-  const [audioFile, setAudioFile] = useState<File | null>(null);
+  const [mediaFile, setMediaFile] = useState<File | null>(null);
+  const [segments, setSegments] = useState<Segment[]>(JSON.parse(localStorage.getItem('transcribe-ai') || '[]'));
   const [processing, setProcessing] = useState(false);
   const inputElement = useRef<HTMLInputElement>(null)
 
   const onChangeAudio = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      setAudioFile(event.target.files[0]);
+      setMediaFile(event.target.files[0]);
     }
   };
 
   const resetForm = () => {
-    setAudioFile(null)
+    setMediaFile(null)
     if (inputElement.current) {
       inputElement.current.value = ''
     }
   }
   const onProcess = async () => {
-    if (audioFile) {
+    if (mediaFile) {
       setProcessing(true);
       try {
         const service = new TranscribeAIService()
-        const segments = await service.transcribe(audioFile)
+        const segments = await service.transcribe(mediaFile)
         localStorage.setItem('transcribe-ai', JSON.stringify(segments))
+        setSegments(segments)
         console.log(segments)
       } catch (exception) {
         console.error(exception)
@@ -47,18 +52,32 @@ export const Dashboard = () => {
   };
   return (
     <div className="flex w-full min-h-screen bg-background text-foreground font-sans items-center justify-center">
+      {
+        mediaFile && (
+          <video id="video" controls preload="metadata">
+            <source src={URL.createObjectURL(mediaFile)} type="video/mp4" />
+            <track
+              kind="subtitles"
+              label="English"
+              srcLang="en"
+              default
+              src={URL.createObjectURL(subtitleService.processVttFile(segments))} />
+          </video>
+        )
+      }
+      
       <div className="mt-auto mb-auto ml-auto mr-auto">
         <Card className="max-w-[350px]">
           <CardHeader>
             <CardTitle>Transcribe.ai</CardTitle>
-            <CardDescription>Transcribe audio in one-click.</CardDescription>
+            <CardDescription>Transcribe videos in one-click.</CardDescription>
           </CardHeader>
           <CardContent>
             <form>
-              <Label htmlFor="audio">Audio file</Label>
+              <Label htmlFor="audio">Video file</Label>
               <div className="flex gap-2">
                 <Input id="audio" type="file" onChange={onChangeAudio} ref={inputElement}/>
-                {audioFile && (
+                {mediaFile && (
                   <Button
                     variant="outline"
                     type="button"
