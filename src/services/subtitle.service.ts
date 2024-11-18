9c1438e5e031e64fe6d38b52643ea821befd7612
Filipe Melo <filipe.silva@encora.com>
2024-11-18 16:36:33 -0300

    feat: create VTT subtitle service

---
+++ src/services/subtitle.service.ts
@@ -0,0 +1,44 @@
+import { Segment } from "./transcribe-ai.service";
+
+export class SubtitleService {
+  processVttFile(segments: Segment[]) {
+    let body = `WEBVTT`;
+    
+    let cuesCount = 1
+
+    for (const segment of segments) {
+      const speakerId = segment.speaker_id;
+      
+      for (const transcription of segment.transcriptions) {
+
+        const startStr = this.secondsToString(transcription.start)
+        const endStr = this.secondsToString(transcription.end)
+        body += `
+
+        ${cuesCount}
+        ${startStr} --> ${endStr} align:center
+        <v ${speakerId}>${transcription.word.trim()}</v>`
+        cuesCount++
+      }
+    }
+    console.log(body)
+    return new Blob([body], { type: "text/plain" });
+  }
+
+  private secondsToString(value: number) {
+    const hours = value / 3600;
+
+    const minutes = (hours - Math.trunc(hours)) * 60;
+
+    const seconds = (minutes - Math.trunc(minutes)) * 60;
+
+    const miliseconds = seconds - Math.trunc(seconds);
+
+    return `${String(Math.trunc(hours)).padStart(2, "0")}:${String(
+      Math.trunc(minutes)
+    ).padStart(2, "0")}:${String(Math.trunc(seconds)).padStart(
+      2,
+      "0"
+    )}.${String(miliseconds.toFixed(3)).replace("0.", "")}`;
+  }
+}
